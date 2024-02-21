dbebc277cf544efb0d589305f47521eca3cab9a3
Gayathri Swaminathan <gayathriswami@Gayathris-MacBook-Air.local>
2024-02-21 18:48:35 +0530

    add: index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,100 @@
+// 1) import react and ReactDOM libraries
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 2) get refernce to the div with ID root
+
+const el = document.getElementById('root');
+//3) tell react to take control of that element, tells application in this root
+
+const root = ReactDOM.createRoot(el);
+
+
+//4) create a component
+function App(){
+    //doesn't show if before return
+    <h2>hey</h2>
+    const type = "number"
+    const min = 3
+    const max = 10
+    return (
+    <div>
+        <Hello/> 
+        <Food/>
+        <Experiment/>
+        <Testing/>
+        <Working/>
+        <Textarea/>
+        <input  style = {{border: '3px dotted red'}} type={type} min={min} max ={max}/>
+        <h1> Heeloo</h1>
+    </div>)
+    
+}
+function Hello(){
+    return(<div>
+        <ul>
+        <li style = {{textDecoration : "underline", color:"blue", width:"50px", border:"2px solid green"}}>food</li>
+        <li>water</li>
+    </ul>
+   
+    <ol>
+        <li>lunch</li>
+        <li>dinner</li>
+    </ol>
+    <h3>data for monday</h3>
+    </div>)
+}
+
+function Food(){
+    return(
+        <div className = "hello" style = {{backgroundColor:'INDIGO', padding:'10px', color:'white'}}>
+            <h5>detailed menu</h5>
+            <hr></hr>
+            <h6>rice, curry, chutney, salad, sambar</h6>
+
+        </div>
+    )
+}
+function Experiment(){
+    // shows only string and text type else error
+    const word = 'welcome'
+    const num = Math.random() + Math.random()
+    let sum = Math.random() + Math.random()
+    let message = 'Bye there'
+    if (Math.random()>0.5){
+        message = 'Hello there!';
+    }
+    return(
+        <div>
+        <h1>{message}</h1>
+        <h2>{word}</h2>
+        <h2>{num}</h2>
+        <h3>{sum}</h3>
+        </div>
+    )
+}
+function Testing (){
+    const date = new Date()
+    const time = date.toLocaleTimeString();
+
+    return(<div>
+         <h1>{time}</h1>
+         <h1>{new Date().toLocaleString()}</h1></div>)
+}
+
+function Working(){
+    const name = "amm"
+    const age = 14
+    return(
+        <div>{name}{age}</div>
+    )
+
+}
+
+function Textarea(){
+   
+    return <textarea spellCheck ={false}autoFocus={true} maxLength = {10}/>
+}
+// 5) show the component on the screen 
+root. render(<App/>) 
